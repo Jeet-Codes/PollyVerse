19a87021086454c587ee7e1b8ae7f73bb2d4537c
Bikash <bikashmalu1@gmail.com>
2024-04-13 00:58:06 +0530

    shoe case panel is on going

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -66,15 +66,15 @@ const Login = () => {
                 />
               </div>
               <div className="mt-0 flex justify-start mx-4 ml-[30px]">
-               <button class="relative inline-flex h-12 overflow-hidden rounded-full p-[2px] focus:outline-none focus:ring-2 focus:ring-slate-400 focus:ring-offset-2 focus:ring-offset-slate-50">
+               <button class="relative inline-flex h-12 overflow-hidden rounded-xl p-[1px] focus:outline-none focus:ring-1 focus:ring-slate-400 focus:ring-offset-2 focus:ring-offset-slate-150">
                   <span class="absolute inset-[-1000%] animate-[spin_2s_linear_infinite] bg-[conic-gradient(from_90deg_at_50%_50%,#E2CBFF_0%,#393BB2_50%,#E2CBFF_100%)]" />
-                  <span class="inline-flex h-full w-full cursor-pointer items-center justify-center rounded-full bg-slate-950 px-3 py-1 text-sm font-medium text-white backdrop-blur-3xl">
+                  <span class="inline-flex h-full w-full cursor-pointer items-center justify-center rounded-xl bg-slate-950 px-3 text-sm font-medium text-white backdrop-blur-3xl">
                   <Link to={'/home'}>Login Here!!</Link>  
                   </span>
                 </button> 
-               <button class=" mx-1 relative inline-flex h-12 overflow-hidden rounded-full p-[2px] focus:outline-none focus:ring-2 focus:ring-slate-400 focus:ring-offset-2 focus:ring-offset-slate-50">
+               <button class=" mx-3 relative inline-flex h-12 overflow-hidden rounded-xl p-[1px] focus:outline-none focus:ring-2 focus:ring-slate-400 focus:ring-offset-2 focus:ring-offset-slate-50">
                   <span class="absolute inset-[-1000%] animate-[spin_2s_linear_infinite] bg-[conic-gradient(from_90deg_at_50%_50%,#E2CBFF_0%,#393BB2_50%,#E2CBFF_100%)]" />
-                  <span class="inline-flex h-full w-full cursor-pointer items-center justify-center rounded-full bg-slate-950 px-3 py-1 text-sm font-medium text-white backdrop-blur-3xl">
+                  <span class="inline-flex h-full w-full cursor-pointer items-center justify-center rounded-xl bg-slate-950 px-3 text-sm font-medium text-white backdrop-blur-3xl">
                   <Link to={'/show'}>Model view</Link>  
                   </span>
                 </button> 
--- src/components/showcase/ShowCase.jsx
+++ src/components/showcase/ShowCase.jsx
@@ -1,16 +1,36 @@
 import React from "react";
 import { cn } from "../utils/cn";
 import Spotlight from "../utils/Spotlight";
+import Videoview from "./Videoview";
 
 export function SpotlightPreview() {
   return (
-    <div className="h-[100vh] w-full rounded-md flex md:items-center md:justify-center bg-black/[0.96] antialiased bg-grid-white/[0.02] relative overflow-hidden">
+    <div className="text-white h-[100%] w-full rounded-md flex md:items-center md:justify-center bg-black/[0.96] antialiased bg-grid-white/[0.02] relative overflow-hidden">
       <Spotlight
         className="-top-40 left-0 md:left-60 md:-top-20"
-        fill="white"
+        fill="blue"
       />
-      <div className="">
+      <div className=" p-4 max-w-7xl  mx-auto relative z-10  w-full pt-20 md:pt-0 mt-10">
+        <h1 className="text-4xl md:text-5xl font-bold text-center bg-clip-text text-transparent bg-gradient-to-b from-neutral-50 to-neutral-400 bg-opacity-50">
+          Hello there!! <br /> All Models Present Below
+        </h1>
+        <div>
+          <h2 className="text-3xl font-semibold italic mt-8 text-blue-500">Mechanical Watch :-</h2>
+          <p className="text-[20px]">Lorem ipsum dolor sit amet consectetur adipisicing elit. Aut iusto nulla veritatis fugiat, aliquam commodi quidem magni, debitis fugit nam corporis ratione rem autem voluptatibus odit consequatur a excepturi vitae error explicabo voluptates aperiam fuga sequi? Quasi, cum nostrum dolor totam eligendi earum debitis! Rem omnis maxime nobis assumenda animi.</p> <br />
+          <p className="text-[20px]">Lorem ipsum dolor sit amet consectetur adipisicing elit. Aperiam sequi, libero quod sed quo modi excepturi qui obcaecati esse architecto.</p>
+          <div className="bg-gray-300 h-52 w-96 mx-auto mt-4">
 
+          </div>
+        </div>
+        <div>
+          <h2 className="text-3xl font-semibold italic mt-8 text-blue-500">Mechanical Robot:-</h2>
+          <p className="text-[20px]">Lorem ipsum dolor sit amet consectetur adipisicing elit. Aut iusto nulla veritatis fugiat, aliquam commodi quidem magni, debitis fugit nam corporis ratione rem autem voluptatibus odit consequatur a excepturi vitae error explicabo voluptates aperiam fuga sequi? Quasi, cum nostrum dolor totam eligendi earum debitis! Rem omnis maxime nobis assumenda animi.</p> <br />
+          <p className="text-[20px]">Lorem ipsum dolor sit amet consectetur adipisicing elit. Aperiam sequi, libero quod sed quo modi excepturi qui obcaecati esse architecto.</p>
+          <div className="bg-gray-300 h-52 w-96 mx-auto mt-4">
+
+          </div>
+        </div>
+        
       </div>
     </div>
   );
